7cc407f7f31a344ca897e6558fd1e56b3e62de3b
liwei <1491951751@qq.com>
2024-05-08 17:43:10 +0800

    员工分页展示业务功能开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.EmployeeDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
@@ -21,4 +22,6 @@ public interface EmployeeMapper {
             "create_time,update_time,create_user,update_user)values (#{name},#{username},#{password},#{phone}," +
             "#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+    Page<Employee> pageQuery(String name);
 }
